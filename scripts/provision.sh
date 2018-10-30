2bedfde8e8b7c82d238a622bbd2cc79d55c8e9b5
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-10-30 16:30:05 +0200

    edited scripts/provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,35 +1,39 @@
 #!/usr/bin/env bash
 
-# Update apt and install golang 1.10
-sudo apt-get update
-sudo apt-get install -y golang-1.10
+# make sure apt database is up-to date
+apt-get update
 
-# Install wget if not installed
-which wget || {
-    sudo apt-get update
-    sudo apt-get install -y wget
+# install golang-1.10
+apt-get install -y golang-1.10
+apt-get install -y git
+grep 'GOPATH|GOROOT' ~/.bash_profile &>/dev/null || {
+  sudo mkdir -p ~/go
+  cp ~/.bash_profile ~/.bash_profile.ori
+  grep -v 'GOPATH|GOROOT' ~/.bash_profile.ori | sudo tee -a ~/.bash_profile
+  echo 'export GOROOT=/usr/lib/go-1.10' | sudo tee -a ~/.bash_profile
+  echo 'export PATH=$PATH:$GOROOT/bin' | sudo tee -a ~/.bash_profile
+  echo 'export GOPATH=~/go' | sudo tee -a ~/.bash_profile
 }
 
-# Install unzip if not installed
-which unzip || {
-    sudo apt-get update
-    sudo apt-get install -y unzip
+grep 'GOPATH|GOROOT' /home/vagrant/.bash_profile &>/dev/null || {
+  sudo mkdir -p /home/vagrant/go
+  cp /home/vagrant/.bash_profile /home/vagrant/.bash_profile.ori
+  grep -v 'GOPATH|GOROOT' /home/vagrant/.bash_profile.ori | sudo tee -a /home/vagrant/.bash_profile
+  echo 'export GOROOT=/usr/lib/go-1.10' | sudo tee -a /home/vagrant/.bash_profile
+  echo 'export PATH=$PATH:$GOROOT/bin' | sudo tee -a /home/vagrant/.bash_profile
+  echo 'export GOPATH=/home/vagrant/go' | sudo tee -a /home/vagrant/.bash_profile
+  sudo chown -R vagrant:  /home/vagrant
 }
 
-# Install git if not installed
-which git || {
-    apt-get update
-    apt-get install -y git
+
+# install terraform
+which wget unzip &>/dev/null || {
+  apt-get install -y wget unzip
 }
 
-
-# Configure  go programming language
-export GOROOT=/usr/local/go
-export GOPATH=$HOME
-sudo ln -s /usr/lib/go-1.10/bin/go /usr/bin/go
-
-
-# Install terraform
-wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip -O /tmp/terraform.zip
-cd /tmp/ && sudo unzip terraform.zip
-sudo mv /tmp/terraform /usr/bin/terraform
+which terraform &>/dev/null || {
+  pushd /usr/local/bin
+  wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
+  unzip terraform_0.11.10_linux_amd64.zip
+  popd
+  }
